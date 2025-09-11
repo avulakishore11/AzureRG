f8db2a866cf071d7ec482a5f6edcb52bdac2caa9
avulakishore11 <avulakishore11@gmail.com>
2025-09-11 14:50:37 -0400

    rg created

---
+++ rg.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+# terrafrom remote backend storage
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "dev-rg"
+    storage_account_name  = "sa286"
+    container_name        = "terraform-container"
+    key                   = "state.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+
+resource"resource_group_name" "dev-rg" {
+    name = "dev-rg"
+    location = "east_us"
+
+}
